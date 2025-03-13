82372aaf2516a53196bfd8fb363351eb15e5273a
victorhabila <48987601+victorhabila@users.noreply.github.com>
2025-03-13 17:57:42 +0100

    jwt configuration

--- src/main/java/com/multi_vendo_ecom/ecommerce/multivendor/config/AppConfig.java
+++ src/main/java/com/multi_vendo_ecom/ecommerce/multivendor/config/AppConfig.java
@@ -1,4 +1,68 @@
 package com.multi_vendo_ecom.ecommerce.multivendor.config;
 
+import jakarta.persistence.Id;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
 public class AppConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(management -> management.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                )).authorizeHttpRequests (authorize -> authorize
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/api/products/*/reviews").permitAll()
+                        .anyRequest().permitAll()
+                ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsCofigurationSource()));
+
+        return http.build();
+    }
+
+
+    private CorsConfigurationSource corsCofigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Collections.singletonList("*"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setExposedHeaders(Collections.singletonList("Authorization"));
+                cfg.setMaxAge(3600L);
+                return cfg;
+            }
+            //This method will be used to encode our password
+            @Bean
+            PasswordEncoder passwordEncoder(){
+                return new BCryptPasswordEncoder();
+
+            }
+
+            // for communicating with external services, we need a rest template
+            @Bean
+            public RestTemplate restTemplate(){
+                return new RestTemplate();
+            }
+
+        };
+    }
 }
